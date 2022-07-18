050509316240258817e59c1e68d9265152430b4f
Emil Petrusenko <74240436+Snechko@users.noreply.github.com>
2022-07-18 19:51:46 +0300

    Delete makePublic directory

--- makePublic/cursorPos.py
+++
@@ -1,6 +0,0 @@
-import pyautogui
-import time
-
-while 1:
-    pyautogui.displayMousePosition()
-    time.sleep(1)
--- makePublic/test.py
+++
@@ -1,6 +0,0 @@
-import pyautogui
-import time
-
-time.sleep(2)
-iml = pyautogui.screenshot(region = (1800, 200, 100 ,880))
-iml.save(r"D:\Desktop\makePublic\screen.png")
